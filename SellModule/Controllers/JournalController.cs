c8e778315116abc73bb666c13e46fcf1789db814
MIX_Frog <mixfrog91@gmai.com>
2017-06-30 08:36:33 +0500

    Commit

---
+++ SellModule/Controllers/JournalController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SellModule.Models;
+
+namespace SellModule.Controllers
+{
+    public class JournalController : Controller
+    {
+        ContractContext db = new ContractContext();
+
+        public ViewResult ViewJournal()
+        {
+            return View(db.Contracts.ToList());
+        }
+
+        public ViewResult Edit()
+        {
+            return View();
+        }
+
+        public ViewResult Delete()
+        {
+            return View();
+        }
+}
+}
